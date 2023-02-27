96a702212d6f179fa11ba96363c3cb7339541baf
rajendrabadgujar <rajendra.badgujar@everestek.com>
2023-02-27 14:57:49 +0530

    q

--- conf/gunicorn_config.py
+++ conf/gunicorn_config.py
@@ -1,6 +1,6 @@
-command = '/home/ubuntu/django_env/bin/gunicorn'
+command = '/var/lib/jenkins/workspace/django_env/bin/gunicorn'
 pythonpath = '/home/ubuntu/myproject'
 bind = '0.0.0.0:8000'
 workers = 3
 
-#/home/ubuntu/django_env/bin/gunicorn --bind 0.0.0.0:8000 myproject.wsgi
+#/var/lib/jenkins/workspace/django_env/bin/gunicorn --bind 0.0.0.0:8000 myproject.wsgi
