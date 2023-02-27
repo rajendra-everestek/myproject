2b409d24d9cafd07a7209c41dc4a4d881d784208
rajendrabadgujar <rajendra.badgujar@everestek.com>
2023-02-27 13:40:33 +0530

    q

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,8 +1,6 @@
 sudo rm -rf /etc/nginx/sites-enabled/myproject
 
-sudo cp -rf /var/lib/jenkins/workspace/My-Project/nginx/nginx.conf /etc/nginx/sites-available/djangojenkin
-
-sudo ln -s /etc/nginx/sites-available/myproject
+sudo cp -rf /var/lib/jenkins/workspace/My-Project/nginx/nginx.conf /etc/nginx/sites-available/myproject
 
 sudo ln -s /etc/nginx/sites-available/myproject /etc/nginx/sites-enabled
 
