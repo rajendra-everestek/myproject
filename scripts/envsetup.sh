3be8709c4e1c116390e7ee3740620eda08a072f8
rajendrabadgujar <rajendra.badgujar@everestek.com>
2023-02-27 12:19:38 +0530

    q

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
+
 sudo apt install nginx -y
+
 sudo apt install -y python3-venv
-# python3 -m venv /home/ubuntu/django_env
-# source /home/ubuntu/django_env/bin/activate
-# pip install django
-# pip install gunicorn
+
+python3 -m venv /var/lib/jenkins/workspace/django_env
+
+source /var/lib/jenkins/workspace/django_env/bin/activate
+
+pip install django
+
+pip install gunicorn
