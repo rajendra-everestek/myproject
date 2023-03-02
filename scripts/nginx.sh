1cdedf9dd158d902c773242cf969002e76fb4cde
rajendrabadgujar <rajendra.badgujar@everestek.com>
2023-03-02 19:49:15 +0530

    Q

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -5,5 +5,4 @@ sudo cp -rf /var/lib/jenkins/workspace/My-Project/nginx/nginx.conf /etc/nginx/si
 
 sudo ln -s /etc/nginx/sites-available/myproject /etc/nginx/sites-enabled
 
-
 sudo systemctl restart nginx
